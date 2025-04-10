80f0ed9ad8e251265c97a2513a3c0a6e87d20b7f
PauloCesar <pauloandradeoliveira2004@gmail.com>
2025-04-10 16:22:33 -0300

    first commit

---
+++ 01.05. Pacotes/src/Principal.java
@@ -0,0 +1,50 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("\n============================================");
+        System.out.println("Conta titular 1");
+        System.out.println("============================================");
+        // criando a Pessoa titular1.
+        Pessoa titular1 = new Pessoa();
+        titular1.setNome("Gabrielle");
+        titular1.setDocumento("5006361847");
+
+        //Criando a conta do titular1.
+        //Istanciando a classe Conta com o construtor sem argumento
+        Conta contaTitular1 = new Conta(titular1, 666, 224, 1000);
+//        contaTitular1.setTitular(titular1);
+//        contaTitular1.setAgencia(555);
+//        contaTitular1.setNumero(123);
+//        contaTitular1.setSaldo(500); // Titular 1 inicia com um saldo de R$ 500,00.
+
+        System.out.println("O tiular da conta é: " + contaTitular1.getTitular().getNome());
+        System.out.println("Documento: " + titular1.getDocumento());
+        System.out.println("Agencia: " + contaTitular1.getAgencia());
+        System.out.println("Numero: " + contaTitular1.getNumero());
+        System.out.println("Saldo atual: R$ " + contaTitular1.getSaldo());
+        //Invocando os metodos
+        contaTitular1.depositarDinhero(500);
+
+
+        System.out.println("\n============================================");
+        System.out.println("Conta titular 2");
+        System.out.println("============================================");
+
+
+        //Istanciando a classe Conta com o construtor com valor ja definidos.
+        Pessoa titular2 = new Pessoa();
+        titular2.setNome("Paulo");
+        titular2.setDocumento("5006361847");
+
+        Conta contaTitular2 = new Conta(titular2, 555, 124, 1000);
+        System.out.println("O titular da conta e: " + contaTitular2.getTitular().getNome());
+        System.out.println("Documento: " + titular2.getDocumento());
+        System.out.println("Agencia: " + contaTitular2.getAgencia());
+        System.out.println("Numero: " + contaTitular2.getNumero());
+        System.out.println("Saldo inicial sem alteracao: R$ " + contaTitular2.getSaldo());
+        //Invocando o metodo de depositar na contaTitulae2
+        contaTitular2.depositarDinhero(500);
+        contaTitular2.sacarDinheiro(500);
+    }
+
+}
